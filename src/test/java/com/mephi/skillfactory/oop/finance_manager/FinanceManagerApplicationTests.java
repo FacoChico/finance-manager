127b6c2a4426435b985ace499531a1d122faac61
grigorypopov <popov098097grigory@gmail.com>
2025-11-09 20:31:12 +0300

    Initial commit

---
+++ src/test/java/com/mephi/skillfactory/oop/finance_manager/FinanceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mephi.skillfactory.oop.finance_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
